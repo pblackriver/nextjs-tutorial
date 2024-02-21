2faa54780d855a988503220de884fa32152995ab
Paulo Preto <paulopreto.dev@gmail.com>
2024-02-21 00:17:49 +0000

    add auth with next-auth

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -15,7 +15,6 @@ import { useFormState } from "react-dom";
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState = { message: null, errors: {} };
   const [state, dispatch] = useFormState(createInvoice, initialState);
-  console.log(state);
 
   return (
     <form action={dispatch}>
